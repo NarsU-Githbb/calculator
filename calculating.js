3ceccfe5f19a56b940ef8aaa86d4ab86c3fa06ab
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-04-28 11:40:37 +0300

    Create Live Screen & Responsive Buttons
    Made connections between buttons & the live screen, so that every button you press will print corresponding icon that
    is written in the button itself.
    
    Will change the function on "= & C" later, but next I'm trying to make the live screen to receive strings,
    since right now it's only receiving a single icon at the time.

--- calculating.js
+++ calculating.js
@@ -1,1 +1,69 @@
-function plus()
+let showCalculating = document.getElementById('display-screen');
+
+
+
+const buttonOne = document.getElementById('first');
+buttonOne.addEventListener('click', function() { 
+    showCalculating.textContent = "1";
+});
+
+const buttonTwo = document.getElementById('second');
+buttonTwo.addEventListener('click', function() { 
+    showCalculating.textContent = "2";
+});
+const buttonThree = document.getElementById('third');
+buttonThree.addEventListener('click', function() { 
+    showCalculating.textContent = "3";
+});
+const buttonFour = document.getElementById('fourth');
+buttonFour.addEventListener('click', function() { 
+    showCalculating.textContent = "4";
+});
+const buttonFive = document.getElementById('fifth');
+buttonFive.addEventListener('click', function() { 
+    showCalculating.textContent = "5";
+});
+const buttonSix = document.getElementById('sixth');
+buttonSix.addEventListener('click', function() { 
+    showCalculating.textContent = "6";
+});
+const buttonSeven = document.getElementById('seventh');
+buttonSeven.addEventListener('click', function() { 
+    showCalculating.textContent = "7";
+});
+const buttonEight = document.getElementById('eigth');
+buttonEight.addEventListener('click', function() { 
+    showCalculating.textContent = "8";
+});
+const buttonNine = document.getElementById('ninth');
+buttonNine.addEventListener('click', function() { 
+    showCalculating.textContent = "9";
+});
+const buttonNada = document.getElementById('zero');
+buttonNada.addEventListener('click', function() { 
+    showCalculating.textContent = "0";
+});
+const buttonPlus = document.getElementById('add');
+buttonPlus.addEventListener('click', function() { 
+    showCalculating.textContent = "+";
+});
+const buttonMinus = document.getElementById('reduce');
+buttonMinus.addEventListener('click', function() { 
+    showCalculating.textContent = "-";
+});
+const buttonCross = document.getElementById('multiply');
+buttonCross.addEventListener('click', function() { 
+    showCalculating.textContent = "x";
+});
+const buttonSlice = document.getElementById('divide');
+buttonSlice.addEventListener('click', function() { 
+    showCalculating.textContent = "÷";
+});
+const buttonResult = document.getElementById('equals');
+buttonResult.addEventListener('click', function() { 
+    showCalculating.textContent = "=";
+});
+const buttonRestart = document.getElementById('clear');
+buttonRestart.addEventListener('click', function() { 
+    showCalculating.textContent = "C";
+});
